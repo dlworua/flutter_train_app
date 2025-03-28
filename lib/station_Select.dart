90ba8b9414e711aa6cfef9485f7432da952c7fdc
dlworua <leegaegyum@gmail.com>
2025-03-28 18:33:17 +0900

    feat: '출발역', '도착역' , 세로선, '선택' 생성 후 가운데 정렬

--- lib/station_Select.dart
+++ lib/station_Select.dart
@@ -5,10 +5,40 @@ class StationSelect extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: double.infinity,
       height: 200,
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
+        //글자 및 세로선 가운데 정렬
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //"출발역" 텍스트 생성(사이즈 '15', 색상 'grey', 글꼴 굵게 설정)
+          Text(
+            '도착역',
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.grey,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          //"선택" 텍스트 생성(폰트사이즈 '40' 설정)
+          Text('선택', style: TextStyle(fontSize: 40)),
+          //세로선 생성 (너비 '2', 높이 '50', 색상 'Colors.grey[400]'설정)
+          Container(width: 2, height: 50, color: Colors.grey[400]),
+          Text(
+            '출발역',
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.grey,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          //"선택" 텍스트 생성(폰트사이즈 '40' 설정)
+          Text('선택', style: TextStyle(fontSize: 40)),
+        ],
       ),
     );
   }
