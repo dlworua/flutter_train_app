328b3974fa4b585e44883253a6ed5060c99a5e87
dlworua <leegaegyum@gmail.com>
2025-03-28 16:04:05 +0900

    feat:HomePage 클래스 위젯 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_train_app/station_select.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(home: HomePage());
   }
 }
+
+//HomePage 클래스 위젯 생성
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(children: [StationSelect()]),
+    );
+  }
+}
+
+/*
+-Scaffold
+ -AppBar
+  -title
+   -Text
+ -body
+  -colum
+  */
