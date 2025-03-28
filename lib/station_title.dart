91558e299fea1213fdc10a076012da02e444700d
dlworua <leegaegyum@gmail.com>
2025-03-28 16:09:33 +0900

    feat:app bar  stationTitle 생성

---
+++ lib/station_title.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+//app bar  stationTitle 생성
+class StationTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('기차 예매');
+  }
+}
