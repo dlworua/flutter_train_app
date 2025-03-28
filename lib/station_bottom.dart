4167e9c48749b1fbcb937166a427a8d193ac7ab3
dlworua <leegaegyum@gmail.com>
2025-03-28 19:57:26 +0900

    feat: StationBottom 파일 생성

---
+++ lib/station_bottom.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class StationBottom extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.blue, height: 250);
+  }
+}
