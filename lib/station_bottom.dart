f8b50479a48bc73f56dd309fafa9ad78feefdfcc
dlworua <leegaegyum@gmail.com>
2025-03-28 23:05:41 +0900

    feat: 좌석선택 UI 구현 완료

--- lib/station_bottom.dart
+++ lib/station_bottom.dart
@@ -3,6 +3,26 @@ import 'package:flutter/material.dart';
 class StationBottom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: double.infinity,
+      height: 55,
+      decoration: BoxDecoration(
+        color: Colors.purple,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '좌석 선택',
+            style: TextStyle(
+              fontSize: 18,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
